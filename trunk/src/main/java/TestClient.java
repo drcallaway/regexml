eb83e28e2a335090fa84bf0ad60a7d60fd545265
drcallaway <drcallaway@7e1c4219-592f-d918-1099-41ed9bd13f3a>
2010-04-02 17:40:41 +0000

    Added.

---
+++ trunk/src/main/java/TestClient.java
@@ -0,0 +1,38 @@
+import org.regexml.ExpressionFactory;
+import org.regexml.resource.FileSystemResource;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Temporary test client.
+ */
+public class TestClient
+{
+    public static void main(String[] args)
+    {
+        ExpressionFactory ef = new ExpressionFactory(new FileSystemResource("test.xml"));
+
+        Pattern pattern = ef.getExpression("phone");
+        Matcher matcher = pattern.matcher("(801) 796-3438");
+
+        if (matcher.find())
+        {
+            for (int i = 1; i <= matcher.groupCount(); i++)
+            {
+                System.out.println(matcher.group(i));
+            }
+        }
+
+        pattern = ef.getExpression("url");
+        matcher = pattern.matcher("http://www.regexml.org:8080/test?param=1#anchor");
+
+        if (matcher.find())
+        {
+            for (int i = 1; i <= matcher.groupCount(); i++)
+            {
+                System.out.println(matcher.group(i));
+            }
+        }
+    }
+}

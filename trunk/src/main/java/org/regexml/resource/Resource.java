c38eafe5d52da0c9bbaec8b839b699ede47a804a
drcallaway <drcallaway@7e1c4219-592f-d918-1099-41ed9bd13f3a>
2010-04-02 16:18:54 +0000

    Updated javadocs.

--- trunk/src/main/java/org/regexml/resource/Resource.java
+++ trunk/src/main/java/org/regexml/resource/Resource.java
@@ -3,11 +3,7 @@ package org.regexml.resource;
 import java.io.Reader;
 
 /**
- * Created by IntelliJ IDEA.
- * User: dcallaway
- * Date: Apr 1, 2010
- * Time: 3:30:18 PM
- * To change this template use File | Settings | File Templates.
+ * Represents a configuration resource containing XML expressions.
  */
 public interface Resource
 {

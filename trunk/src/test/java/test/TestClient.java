fd86fc9119944770aaa30073868aaf2d014d9b9b
drcallaway <drcallaway@7e1c4219-592f-d918-1099-41ed9bd13f3a>
2010-04-22 15:54:11 +0000

    Moved packages (to simplify generating of javadocs) and switched from phone to zipcode example.

---
+++ trunk/src/test/java/test/TestClient.java
@@ -0,0 +1,47 @@
+package test;
+
+import org.regexml.Expression;
+import org.regexml.ExpressionFactory;
+import org.regexml.resource.FileSystemResource;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Temporary test client.
+ */
+public class TestClient
+{
+    public static void main(String[] args)
+    {
+        ExpressionFactory ef = new ExpressionFactory(new FileSystemResource("test.xml"), true);
+
+        Expression exp = ef.getExpression("zipcode");
+
+        System.out.println("RegEx: " + exp.getRegExString());
+        Pattern pattern = exp.getPattern();
+        Matcher matcher = pattern.matcher("98765-4321");
+
+        if (matcher.find())
+        {
+            for (int i = 1; i <= matcher.groupCount(); i++)
+            {
+                System.out.println(matcher.group(i));
+            }
+        }
+
+        exp = ef.getExpression("url");
+
+        System.out.println("RegEx: " + exp.getRegExString());
+        pattern = exp.getPattern();
+        matcher = pattern.matcher("http://www.regexml.org:8080/sample/resource?param=1#anchor");
+
+        if (matcher.find())
+        {
+            for (int i = 1; i <= matcher.groupCount(); i++)
+            {
+                System.out.println(matcher.group(i));
+            }
+        }
+    }
+}
